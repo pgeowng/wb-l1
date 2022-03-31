c46016790e59bc02cba53853c85116f78b9694ce
umgi <gtdsocial@icloud.com>
2022-03-31 22:43:22 +0300

    add: 19

--- tasks/19/main.go
+++ tasks/19/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
+func Reverse(arr []rune) []rune {
+	last := len(arr) - 1
+	mid := len(arr) / 2
+	for i := 0; i < mid; i++ {
+		arr[i], arr[last-i] = arr[last-i], arr[i]
+	}
+	return arr
+}
+
+// Считывает построчно stdin. Переворачивает каждую строку.
+// Проверка:
+//   $ cat main.go | go run main.go
+//   $ echo "hello\nworld\nглаврыба\n日本語" | go run main.go
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	line := make([]rune, 0, 10)
+
+	for {
+		r, _, err := reader.ReadRune()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "reverse: %v\n", err)
+			os.Exit(1)
+		}
+
+		if r == '\n' {
+			fmt.Println(string(Reverse(line)))
+			line = line[:0]
+		} else {
+			line = append(line, r)
+		}
+
+	}
+
+	fmt.Print(string(Reverse(line)))
 }

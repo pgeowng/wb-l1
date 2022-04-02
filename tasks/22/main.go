9a17abf9c8fb26a5cfd36c583828d8f175f96b27
umgi <gtdsocial@icloud.com>
2022-04-02 21:51:03 +0300

    add: 22 using math.Big

--- tasks/22/main.go
+++ tasks/22/main.go
@@ -1,62 +1,54 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"math/big"
+	"os"
+	"strconv"
 )
 
-type Digit int
-
-const base = 1000000000
-
-// func add(a, b []Digit) []Digit {
-
-// }
-
-// func toDigits(n string) []Digit {
-
-// }
-
-func calc(op string, a string, b string) (result *string, err error) {
-	switch op {
-	case "+":
-	case "-":
-	case "*":
-	case "/":
-	default:
-		return nil, errors.New("unknown operation")
-	}
-	return nil, nil
-}
-
-func validateNum(n string) error {
-	for _, rune := range n {
-		if rune < '0' || '9' < rune {
-			return errors.New("number contains non digit rune: " + string(rune))
-		}
-	}
-	return nil
-}
+// echo "+ -9_223_372_036_854_775_808 9_223_372_036_854_775_807" | go run main.go
 
 func main() {
 	var op, a, b string
 
 	_, err := fmt.Scanf("%s %s %s", &op, &a, &b)
 	if err != nil {
-		fmt.Println("error: ", err)
+		fmt.Println("error:", err)
 		fmt.Println("format: <op> <num1> <num2>")
-		return
+		os.Exit(1)
 	}
 
-	if err := validateNum(a); err != nil {
+	a64, err := strconv.ParseInt(a, 0, 64)
+	if err != nil {
 		fmt.Println("first number error:", err)
-		return
+		os.Exit(1)
 	}
 
-	if err := validateNum(b); err != nil {
+	b64, err := strconv.ParseInt(b, 0, 64)
+	if err != nil {
 		fmt.Println("second number error:", err)
-		return
+		os.Exit(1)
 	}
 
-	fmt.Printf("##%s##%s##%s##", op, a, b)
+	bigA := big.NewInt(a64)
+	bigB := big.NewInt(b64)
+
+	bigResult := big.NewInt(0)
+
+	switch op {
+	case "+":
+		bigResult = bigResult.Add(bigA, bigB)
+	case "-":
+		bigResult = bigResult.Sub(bigA, bigB)
+	case "*":
+		bigResult = bigResult.Mul(bigA, bigB)
+	case "/":
+		bigResult = bigResult.Div(bigA, bigB)
+	default:
+		fmt.Println("unknown operation:", op)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s %s %s = %s\n", bigA, op, bigB, bigResult)
 }
